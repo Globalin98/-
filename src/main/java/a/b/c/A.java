4fc5829e4347ef9b17fe4a4927cb542a5fcdfb34
USER <bca229873@gmail.com>
2025-09-24 18:19:37 +0900

    jsp과정

---
+++ src/main/java/a/b/c/A.java
@@ -0,0 +1,22 @@
+package a.b.c;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class A extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) 
+			throws ServletException, IOException {
+
+		System.out.println("A doget");
+
+		resp.sendRedirect("/SERVLET_PROJECT/def");
+
+	}
+}
