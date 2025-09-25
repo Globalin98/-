4986ee7503fb7b142cf8b4917b2c72f0d26aeb78
Globalin98 <bca229873@gmail.com>
2025-09-25 16:54:56 +0900

    직접 만든 MVC_PROJECT
    MVC를 마스터 해보자....

---
+++ MVC_PROJECT/src/main/java/com/melon/peace/controller/MusicController.java
@@ -0,0 +1,36 @@
+package com.melon.peace.controller;
+
+import java.io.IOException;
+
+import com.melon.peace.model.MusicModel;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class MusicController extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		doPost(req, resp);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		System.out.println(this);
+		String uri = req.getRequestURI();
+		if (uri.contains("music_write_form.melon")) {
+			req.getRequestDispatcher("WEB-INF/view/music/music_write_form.jsp").forward(req, resp);
+
+		}
+		if (uri.contains("music_write.melon")) {
+			MusicModel.writeMusic(req);
+			resp.sendRedirect("/MVC_PROJECT/");
+		}
+		if (uri.contains("music_list.melon")) {
+			req.setAttribute("musicList", MusicModel.listMusic(req));
+			req.getRequestDispatcher("WEB-INF/view/music/music_list.jsp").forward(req, resp);
+		}
+	}
+}
---
+++ MVC_PROJECT/src/main/java/com/melon/peace/util/OracleConnection.java
@@ -0,0 +1,33 @@
+package com.melon.peace.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class OracleConnection {
+	
+	private static Connection conn;
+	
+	private OracleConnection() {}
+		
+	public static Connection getConnection() {
+		
+		try {
+			if( null == conn ) {
+				Class.forName("oracle.jdbc.driver.OracleDriver");
+				System.out.println("success");
+				
+				conn = DriverManager.getConnection(
+						"jdbc:oracle:thin:@localhost:1521:xe"
+						, "MUSIC"
+						, "1234");
+				System.out.println(conn);
+			
+		}
+
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		return conn;
+	}
+
+}
